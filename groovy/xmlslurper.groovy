ac495fbab1faf0699e9c490aefe1e000d252efaf
yamap55 <tetuya120@hotmail.com>
2016-06-17 23:04:38 +0900

    追加。

--- groovy/xmlslurper.groovy
+++ groovy/xmlslurper.groovy
@@ -3,10 +3,17 @@ def text = """
     <technology>
         <name>Groovy</name>
     </technology>
+    <technology>
+        <name>Java</name>
+    </technology>
+    <database>
+        <name>postgresql</name>
+    </database>
 </list>
 """
 
 def list = new XmlSlurper().parseText(text) 
 
 assert list instanceof groovy.util.slurpersupport.GPathResult 
-assert list.technology.name == 'Groovy' 
+assert list.technology[0].name == 'Groovy' 
+assert list.'*'.name.size() == 3
