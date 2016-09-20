02feb802721df9cc50b7a433d72d98c5b4c91efe
yamap55 <tetuya120@hotmail.com>
2016-09-20 23:28:04 +0900

    [add]ES2015 example

--- javascript/ES2015/code.js
+++ javascript/ES2015/code.js
@@ -1,18 +1,28 @@
 'use strict';
 
+// const and let
 const a = "a";
 // a = "b" // error
 let b = "b";
 b = "c"; // OK
 
+// Template literals
 console.log(`a : ${a}`);
 
-// ----
+// Math.pow
 
 console.log(6 ** 2); // 36
 
-// ----
+// for of
 
 for (const arg of [1,3,5]) {
   console.log(arg); // 1, 3, 5
 }
+
+// default args
+
+function func(a,b="bb") {
+  console.log(b);
+}
+
+func(); // bb
