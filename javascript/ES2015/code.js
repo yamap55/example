04697cde1ca847501c52d11117cbd41a8453cea6
yamap55 <tetuya120@hotmail.com>
2016-09-21 23:04:42 +0900

    [add]es2015 rest parameters

--- javascript/ES2015/code.js
+++ javascript/ES2015/code.js
@@ -10,19 +10,22 @@ b = "c"; // OK
 console.log(`a : ${a}`);
 
 // Math.pow
-
 console.log(6 ** 2); // 36
 
 // for of
-
 for (const arg of [1,3,5]) {
   console.log(arg); // 1, 3, 5
 }
 
 // default args
-
 function func(a,b="bb") {
   console.log(b);
 }
-
 func(); // bb
+
+// Rest parameters
+function test(a, ...b) {
+  console.log(a, b);
+}
+test(); // undefined []
+test(1, 2, 3);// => 1 [2, 3]
