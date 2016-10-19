d22680d0c7b650e0de7b95bf6ff646e55009f31a
yamap55 <tetuya120@hotmail.com>
2016-10-20 00:15:31 +0900

    [update] add lombok annotation

--- lombok/src/main/java/com/yamap55/lombok/Hoge.java
+++ lombok/src/main/java/com/yamap55/lombok/Hoge.java
@@ -2,14 +2,19 @@ package com.yamap55.lombok;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-@ToString
-@Data
+@AllArgsConstructor // Hoge(int id, String name, List<String> list)
+@NoArgsConstructor // Hoge()
+@ToString // toString
+@Data // getter setter
+@EqualsAndHashCode // equals, hashcode
 public class Hoge {
 	private int id;
 	private String name;
 	private List<String> list;
-
 }
--- lombok/src/main/java/com/yamap55/lombok/Main.java
+++ lombok/src/main/java/com/yamap55/lombok/Main.java
@@ -6,16 +6,20 @@ import lombok.val;
 
 public class Main {
     public static void main(String[] args) {
-    	Hoge hoge = new Hoge();
-    	hoge.setId(10);
-    	hoge.setName("ほげ");
+    	Hoge hoge1 = new Hoge();
 
     	val list = new ArrayList<String>();
     	list.add("a");
     	list.add("b");
-    	hoge.setList(list);
 
-    	System.out.println(hoge);
+    	hoge1.setId(10);
+    	hoge1.setName("ほげ");
+    	hoge1.setList(list);
 
+    	Hoge hoge2 = new Hoge(20, "ほげ2",list);
+
+
+    	System.out.println(hoge1);
+    	System.out.println(hoge2);
     }
 }
