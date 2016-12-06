f151efd299e34c76c6d5ee913edb6c5c7aff07c9
yamap55 <tetuya120@hotmail.com>
2016-12-06 22:40:14 +0900

    [add] groovy list example #3

--- groovy/list.groovy
+++ groovy/list.groovy
@@ -1,7 +1,35 @@
 def list = ["a","b","c"]
 list.each {
-  print it // abc
+  // a
+  // b
+  // c
+  println it
 }
 assert list.collect {it * 2} == ["aa", "bb", "cc"]
 assert list.first() == "a"
 assert list.last() == "c"
+assert list.min() == "a"
+assert list.max() == "c"
+assert list*.plus("z") == ["az", "bz", "cz"]
+assert list.join("@") == "a@b@c"
+assert list.any { it == "a" }
+assert !list.any { it == "z" }
+assert list.every { ["a","b","c"].contains(it) }
+assert !list.every { it == "a" }
+assert list.find { it == "a" } == "a"
+assert list.findAll { it > "a" } == ["b", "c"]
+assert list.inject { total, v -> total + v } == "abc"
+assert list.inject("z") {total, v -> total + v } == "zabc"
+assert list.drop(2) == ["c"]
+assert list.split { it == "a" } == [["a"], ["b", "c"]]
+
+println ("*" * 10)
+
+list.eachWithIndex {v, i->
+  // 0 : a
+  // 1 : b
+  // 2 : c
+  println "${i} : ${v}"
+}
+
+println ("*" * 10)
